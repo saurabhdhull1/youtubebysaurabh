90a95e3622e7cda0c63903531c000c29674e710b
Saurabh dhull <109435045+saurabhdhull1@users.noreply.github.com>
2022-12-12 15:15:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import SearchFeed from "./components/SearchFeed";
 import VideoDetail from "./components/VideoDetail";
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter bashname="/youtubebysaurabh">
     <Box sx={{ backgroundColor: "#000" }}>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/youtubebysaurabh" element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
